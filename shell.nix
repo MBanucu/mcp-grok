10f631ad2227fb64ff069d8247077636f15fe2bb
MBanucu <michael.banucu@googlemail.com>
2025-12-24 18:51:58 +0100

    ci(shell,workflow): matrix CI for python 3.12/3.13 using future-proof shell.nix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,24 @@
-{ pkgs ? import <nixpkgs> {}, dontrunmenu ? "" }:
+{ pkgs ? import <nixpkgs> {}
+, pythonVersion ? "312"
+, dontrunmenu ? ""
+}:
 
+let
+  python = builtins.getAttr ("python" + pythonVersion) pkgs;
+in
 pkgs.mkShell {
   packages = with pkgs; [
+    python
     uv
     nodejs_latest
   ];
 
   shellHook = ''
-    uv sync --all-extras
     export PATH="$PWD/bin:$PATH"
-
+    uv sync --all-extras
     if [ -t 1 ] && [ "${dontrunmenu}" != "1" ]; then
       uv run menu.py
       exit $?
     fi
-'';
+  '';
 }
