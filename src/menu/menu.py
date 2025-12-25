2f71a97311b46a41c17e4425fd2564859b7308df
MBanucu <michael.banucu@googlemail.com>
2025-12-25 00:34:54 +0000

    refactor(menu): remove handle_log_action function to simplify log management

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -7,23 +7,6 @@ from .menu_app import MenuApp
 ANSI_ESCAPE = re.compile(r"\x1B\[[0-?]*[ -/]*[@-~]")
 
 
-def handle_log_action(log_path: str, action: str, title: str) -> None:
-    """Handles either viewing or clearing a log, showing appropriate dialog."""
-    if action == 'view':
-        content = menu_core.log_content(log_path)
-        if content:
-            content = ANSI_ESCAPE.sub('', content[-1000:])
-        else:
-            content = "[Log is empty or does not exist]"
-        message_dialog(title=title, text=content).run()
-    elif action == 'clear':
-        menu_core.clear_log(log_path)
-        message_dialog(
-            title="Log Cleared",
-            text=f"{title} has been cleared."
-        ).run()
-
-
 if __name__ == '__main__':
     state = None
     try:
